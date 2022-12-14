c9fd04cac11405410e77d3a6089ce260e011d09b
lilwosky <woseleyp@gmail.com>
2022-12-13 23:15:40 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putcha('p');
+_putcha('u');
+_putcha('t');
+_putcha('c');
+_putcha('h');
+_putcha('a');
+_putcha('r');
+_putcha('\n');
+return (0);
+}
